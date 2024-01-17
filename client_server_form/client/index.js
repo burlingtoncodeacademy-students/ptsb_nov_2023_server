3e4bbca105e743d70887aecceb26e5a0ea2d4b79
u4ik <insighteuphoric@gmail.com>
2024-01-17 18:53:36 -0500

    templating server

---
+++ client_server_form/client/index.js
@@ -0,0 +1,11 @@
+let form = document.getElementById("search-form");
+let dataDiv = document.getElementById("data");
+let getAllButton = document.getElementById("get-all");
+
+// Form Event + Fetch via PARAM
+
+form.addEventListener("submit", (eventObject) => {
+  eventObject.preventDefault();
+  try {
+  } catch (err) {}
+});
