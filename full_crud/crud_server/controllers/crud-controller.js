f1b8cc7079cb26963a2665fdab6cb4f7d17d7fe3
u4ik <insighteuphoric@gmail.com>
2024-01-18 20:54:28 -0500

    added GET-ALL/GET-BY-ID/POST

--- full_crud/crud_server/controllers/crud-controller.js
+++ full_crud/crud_server/controllers/crud-controller.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-
+const db = require("../mockdb/db.json");
+//
 //? Challenge:
 /*
     - 2 GET requests
@@ -10,5 +11,56 @@ const router = require("express").Router();
         - Provide correct HTTP status code
 
 */
+// GET ALL
+router.get("/all", (req, res) => {
+  try {
+    res.status(200).json({
+      Results: db,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
+// GET By ID
+router.get("/one/:id", (req, res) => {
+  try {
+    let filtered = db.filter((i) => i.id == req.params.id);
+    res.status(200).json({
+      Results: filtered,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
+// POST Create item
+router.post("/create", (req, res) => {
+  try {
+    let myObj = {
+      id: db.length + 1,
+      name: req.body.name,
+      category: req.body.category,
+      price: req.body.price,
+      emoji: req.body.emoji,
+    };
+
+    db.push(myObj);
+
+    res.status(200).json({
+      Added: myObj,
+      Results: db,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      Error: err,
+    });
+  }
+});
 
 module.exports = router;
--- full_crud/crud_server/index.js
+++ full_crud/crud_server/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const crudController = require("./controllers/crud-controller");
-const PORT = 8080;
+const PORT = 9000;
+
+// Required to parse through JSON data that the server receives
+app.use(express.json());
 
 //? Challenge:
 /*
     - Use the controller - providing valid path
 
 */
+app.use(cors());
+app.use("/crud", crudController);
 
 app.listen(PORT, () => {
   console.log(`The server is spinning up at port: ${PORT}`);
